f83f7b0177e640e292e3c4eba6232f1e8ead110e
Tharathep Laohaviroj <tuner.tharathep@gmail.com>
2026-06-17 17:27:18 +0700

    feat: add getComputerChoice()
    - use Math.random() to random return rock, paper, or scissors

---
+++ javascript.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let choice = Math.random();
+    if (choice == 0) {
+        return "rock"
+    }
+    if (choice == 1) {
+        return "paper"
+    }
+    return scissors
+}
+
